12442719a8a99bfa3a3ef01db26815b99d3073a9
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-04-30 14:36:39 +0200

    renaming the CI action and adding a readme

---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+  <a href="https://github.com/lukeed/uvu/actions">
+    <img src="https://github.com/lukeed/uvu/workflows/CI/badge.svg" alt="CI" />
+  </a>
+</div>
+
+# Svelte Entity Store
+
+## WIP: This is a brand new project, investigate at your own risk
--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,17 @@
-
-import sucrase from 'rollup-plugin-sucrase';
+import sucrase from 'rollup-plugin-sucrase'
 import resolve from '@rollup/plugin-node-resolve'
-import pkg from './package.json';
+import pkg from './package.json'
 
 export default {
     input: 'src/index.ts',
     output: [
         { file: pkg.main, format: 'cjs' },
-        { file: pkg.module, format: 'esm' }
+        { file: pkg.module, format: 'esm' },
     ],
     plugins: [
         resolve(),
         sucrase({
-            transforms: ['typescript']
-        })
-    ]
-};
+            transforms: ['typescript'],
+        }),
+    ],
+}
