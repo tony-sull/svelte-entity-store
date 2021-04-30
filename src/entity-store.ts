8a3035a206ed7d5801061cc5e72802b2c997e9b4
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-04-30 15:07:17 +0200

    docs: :memo: Hooking up typedoc
    Adding typedoc to generate docs from JSDoc comments, publishing to github pages

--- src/entity-store.ts
+++ src/entity-store.ts
@@ -1,20 +1,43 @@
 import { writable } from 'svelte/store'
 import { normalize } from './internal/normalize'
-import type { Normalized } from './internal/normalize'
-
 import type { Subscriber, Unsubscriber } from 'svelte/store'
+import type { Normalized } from './internal/normalize'
 
 declare type Invalidator<T> = (value?: T) => void
 declare type Subscribe<T> = (this: void, run: Subscriber<T>, invalidate?: Invalidator<T>) => Unsubscriber
 
+/**
+ * EntityStore supports `string` and `number` values for unique IDs
+ */
 export type ID = string | number
+
+/**
+ * Function used to get the unique ID of an entity
+ */
 export type GetID<T> = (t: T) => ID
 
+/**
+ * Simple Svelte store that normalized data by ID and provides helpers for common data access patterns.
+ */
 export type EntityStore<T> = {
+    /**
+     * Removes all entities
+     */
     reset(): void
+    /**
+     * See (Svelte's docs)[https://svelte.dev/docs#svelte_store] for details on the Store contract and `subscribe` function
+     */
     subscribe: Subscribe<Normalized<T>>
 }
 
+/**
+ * Creates a new entity store
+ *
+ * @typeParam T Entity type being stored
+ * @param getID {@link GetID<T>}
+ * @param initial (optional) Initial array of items to be stored
+ * @returns {@link EntityStore}
+ */
 export function entityStore<T>(getID: GetID<T>, initial: T[] = []): EntityStore<T> {
     const normalizeT = normalize(getID)
 
--- src/internal/normalize.ts
+++ src/internal/normalize.ts
@@ -1,13 +1,29 @@
 import { GetID, ID } from '../'
 
+/**
+ * Normalized state tracking entities by ID
+ */
 export type Normalized<T> = {
+    /**
+     * Map of entities by ID. EntityStore supports `string` and `number` ID types
+     */
     byId: {
         [id: string]: T
         [id: number]: T
     }
+    /**
+     * List of all entities IDs, sorted in the order they were added
+     */
     allIds: ID[]
 }
 
+/**
+ * Takes a list of elements and normalizes them by ID
+ *
+ * @typeParam T Entity type being stored
+ * @param getID {@link GetID<T>}
+ * @returns {@link Normalized<T>} noramlized state holding the given items
+ */
 export const normalize = <T>(getID: GetID<T>) => (items: T[]): Normalized<T> => {
     return items.reduce(
         ({ byId, allIds }, next) => {
